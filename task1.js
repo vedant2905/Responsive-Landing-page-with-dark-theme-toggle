cff5f8c5fd31b9d626b2a1ca7a3ff12207a66fb9
Vedant Vaidya <142279208+vedant2905@users.noreply.github.com>
2024-07-09 16:51:52 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,51 @@
+
+var toggle_btn;
+var big_header;
+var hamburger_menu;
+
+function declare() {
+  toggle_btn = document.querySelector(".toggle-btn");
+  big_header = document.querySelector(".big-header");
+  hamburger_menu = document.querySelector(".hamburger-menu");
+}
+
+const main = document.querySelector("main");
+
+declare();
+
+let dark = false;
+
+function toggleAnimation() {
+  
+  dark = !dark;
+  let clone = big_header.cloneNode(true);
+  if (dark) {
+    clone.classList.remove("light");
+    clone.classList.add("dark");
+  } else {
+    clone.classList.remove("dark");
+    clone.classList.add("light");
+  }
+  clone.classList.add("copy");
+  main.appendChild(clone);
+
+  document.body.classList.add("stop-scrolling");
+
+  clone.addEventListener("animationend", () => {
+    document.body.classList.remove("stop-scrolling");
+    big_header.remove();
+    clone.classList.remove("copy");
+    // Reset Variables
+    declare();
+    events();
+  });
+}
+
+function events() {
+  toggle_btn.addEventListener("click", toggleAnimation);
+  hamburger_menu.addEventListener("click", () => {
+    big_header.classList.toggle("active");
+  });
+}
+
+events();
